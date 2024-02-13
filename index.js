7d47fffa352fdf3d9ec70a9ed86e6a27295988b4
symoon007 <symoonma@gmail.com>
2024-02-13 13:10:24 +0600

    deployed done

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware"); // Import the proxy middleware
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require("cors");
 const categories = require("./data/categories");
 const news = require("./data/news");
 
-app.use(cors());
+// Define the target server URL (replace with your actual server URL)
+const targetServer =
+  "https://the-dragon-news-server-a2s4uzvr5-symoon007s-projects.vercel.app";
+
+// Set up the proxy middleware to forward requests to the target server
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: targetServer, // Target server URL
+    changeOrigin: true, // Change the origin of the host header to the target URL
+    pathRewrite: {
+      "^/api": "/", // Replace "/api" in the request URL with "/"
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Dragon news is coming soooon...");
@@ -18,21 +32,19 @@ app.get("/news", (req, res) => {
 });
 app.get("/news/:id", (req, res) => {
   const id = req.params.id;
- 
-    const selectedId = news.find((n) => n._id === id);
-    res.send(selectedId);
-  
+
+  const selectedId = news.find((n) => n._id === id);
+  res.send(selectedId);
 });
 app.get("/categories/:id", (req, res) => {
   const id = parseInt(req.params.id);
   console.log(id);
   if (id === 0) {
     res.send(news);
-  }else {
-     const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
-  res.send(categoryNews);
+  } else {
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryNews);
   }
- 
 });
 
 app.listen(port, () => {
